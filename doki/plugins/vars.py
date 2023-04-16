8ca737dccbe1fd28e4d7c9c7016ee726eaa19d14
Vikas Ranjit Bhawte <123195203+VikasBhawte@users.noreply.github.com>
2023-04-16 18:31:25 +0530

    Update vars.py

--- doki/plugins/vars.py
+++ doki/plugins/vars.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database.memorydatabase import get_video_limit
-from AnonX.utils.formatters import convert_bytes
+from doki import app
+from doki.misc import SUDOERS
+from doki.utils.database.memorydatabase import get_video_limit
+from doki.utils.formatters import convert_bytes
 
 VARS_COMMAND = get_command("VARS_COMMAND")
 
