1eaeebb52ed86abf9078053eb1a2ef40727c8550
Vikas Ranjit Bhawte <123195203+VikasBhawte@users.noreply.github.com>
2023-04-16 18:24:45 +0530

    Update skip.py

--- doki/plugins/skip.py
+++ doki/plugins/skip.py
@@ -4,16 +4,16 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 import config
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import YouTube, app
-from AnonX.core.call import Anon
-from AnonX.misc import db
-from AnonX.utils.database import get_loop
-from AnonX.utils.decorators import AdminRightsCheck
-from AnonX.utils.inline.play import (stream_markup,
+from doki import YouTube, app
+from doki.core.call import Anon
+from doki.misc import db
+from doki.utils.database import get_loop
+from doki.utils.decorators import AdminRightsCheck
+from doki.utils.inline.play import (stream_markup,
                                           telegram_markup,
                                           close_keyboard)
-from AnonX.utils.stream.autoclear import auto_clean
-from AnonX.utils.thumbnails import gen_thumb
+from doki.utils.stream.autoclear import auto_clean
+from doki.utils.thumbnails import gen_thumb
 
 # Commands
 SKIP_COMMAND = get_command("SKIP_COMMAND")
