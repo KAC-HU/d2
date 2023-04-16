fa34216d82e4f62ea7dd89c7c76d9c328426a8e4
Vikas Ranjit Bhawte <123195203+VikasBhawte@users.noreply.github.com>
2023-04-16 18:22:11 +0530

    Update seek.py

--- doki/plugins/seek.py
+++ doki/plugins/seek.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import YouTube, app
-from AnonX.core.call import Anon
-from AnonX.misc import db
-from AnonX.utils import AdminRightsCheck, seconds_to_min
+from doki import YouTube, app
+from doki.core.call import Anon
+from doki.misc import db
+from doki.utils import AdminRightsCheck, seconds_to_min
 
 # Commands
 SEEK_COMMAND = get_command("SEEK_COMMAND")
