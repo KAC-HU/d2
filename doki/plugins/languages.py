a49f25ba19a5cf370ccb159ece93129f34ed932b
Vikas Ranjit Bhawte <123195203+VikasBhawte@users.noreply.github.com>
2023-04-16 18:12:57 +0530

    Update languages.py

--- doki/plugins/languages.py
+++ doki/plugins/languages.py
@@ -4,9 +4,9 @@ from pyrogram.types import InlineKeyboardButton, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string
-from AnonX import app
-from AnonX.utils.database import get_lang, set_lang
-from AnonX.utils.decorators import (ActualAdminCB, language,
+from doki import app
+from doki.utils.database import get_lang, set_lang
+from doki.utils.decorators import (ActualAdminCB, language,
                                          languageCB)
 
 # Languages Available
