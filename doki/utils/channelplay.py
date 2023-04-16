ac7a7a32dbedff4462c92e3161c620ccdb127fce
Vikas Ranjit Bhawte <123195203+VikasBhawte@users.noreply.github.com>
2023-04-16 17:16:54 +0530

    Update channelplay.py

--- doki/utils/channelplay.py
+++ doki/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonX import app
-from AnonX.utils.database import get_cmode
+from doki import app
+from doki.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
